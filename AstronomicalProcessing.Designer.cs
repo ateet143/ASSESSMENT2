845b99106fd14628d5f77ee8b852b6f5b9eb8b94
Administrator <Administrator@ATITSSD>
2021-09-20 16:57:45 +0800

    Add project files.

---
+++ AstronomicalProcessing.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace AstronomicalProcessing
+{
+    partial class AstronomicalProcessing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBoxHours = new System.Windows.Forms.ListBox();
+            this.TextBoxHours = new System.Windows.Forms.TextBox();
+            this.ButtonAdd = new System.Windows.Forms.Button();
+            this.ButtonModify = new System.Windows.Forms.Button();
+            this.ButtonDelete = new System.Windows.Forms.Button();
+            this.ButtonSort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ButtonSearch = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ButtonAutofill = new System.Windows.Forms.Button();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ListBoxHours
+            // 
+            this.ListBoxHours.FormattingEnabled = true;
+            this.ListBoxHours.Location = new System.Drawing.Point(167, 69);
+            this.ListBoxHours.Name = "ListBoxHours";
+            this.ListBoxHours.Size = new System.Drawing.Size(188, 316);
+            this.ListBoxHours.TabIndex = 0;
+            this.ListBoxHours.Click += new System.EventHandler(this.ListBoxHours_Click);
+            // 
+            // TextBoxHours
+            // 
+            this.TextBoxHours.Location = new System.Drawing.Point(167, 424);
+            this.TextBoxHours.Name = "TextBoxHours";
+            this.TextBoxHours.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxHours.TabIndex = 1;
+            this.TextBoxHours.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBoxHours_KeyPress);
+            // 
+            // ButtonAdd
+            // 
+            this.ButtonAdd.BackColor = System.Drawing.Color.White;
+            this.ButtonAdd.Location = new System.Drawing.Point(22, 70);
+            this.ButtonAdd.Name = "ButtonAdd";
+            this.ButtonAdd.Size = new System.Drawing.Size(75, 42);
+            this.ButtonAdd.TabIndex = 2;
+            this.ButtonAdd.Text = "ADD";
+            this.ButtonAdd.UseVisualStyleBackColor = false;
+            this.ButtonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+            // 
+            // ButtonModify
+            // 
+            this.ButtonModify.BackColor = System.Drawing.Color.White;
+            this.ButtonModify.Location = new System.Drawing.Point(22, 157);
+            this.ButtonModify.Name = "ButtonModify";
+            this.ButtonModify.Size = new System.Drawing.Size(75, 42);
+            this.ButtonModify.TabIndex = 3;
+            this.ButtonModify.Text = "MODIFY";
+            this.ButtonModify.UseVisualStyleBackColor = false;
+            this.ButtonModify.Click += new System.EventHandler(this.ButtonModify_Click);
+            // 
+            // ButtonDelete
+            // 
+            this.ButtonDelete.BackColor = System.Drawing.Color.White;
+            this.ButtonDelete.Location = new System.Drawing.Point(22, 244);
+            this.ButtonDelete.Name = "ButtonDelete";
+            this.ButtonDelete.Size = new System.Drawing.Size(75, 42);
+            this.ButtonDelete.TabIndex = 4;
+            this.ButtonDelete.Text = "DELETE";
+            this.ButtonDelete.UseVisualStyleBackColor = false;
+            this.ButtonDelete.Click += new System.EventHandler(this.ButtonDelete_Click);
+            // 
+            // ButtonSort
+            // 
+            this.ButtonSort.BackColor = System.Drawing.Color.White;
+            this.ButtonSort.Location = new System.Drawing.Point(22, 329);
+            this.ButtonSort.Name = "ButtonSort";
+            this.ButtonSort.Size = new System.Drawing.Size(75, 42);
+            this.ButtonSort.TabIndex = 5;
+            this.ButtonSort.Text = "SORT";
+            this.ButtonSort.UseVisualStyleBackColor = false;
+            this.ButtonSort.Click += new System.EventHandler(this.ButtonSort_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Type Number";
+            // 
+            // ButtonSearch
+            // 
+            this.ButtonSearch.BackColor = System.Drawing.Color.White;
+            this.ButtonSearch.Location = new System.Drawing.Point(280, 423);
+            this.ButtonSearch.Name = "ButtonSearch";
+            this.ButtonSearch.Size = new System.Drawing.Size(75, 20);
+            this.ButtonSearch.TabIndex = 7;
+            this.ButtonSearch.Text = "Search";
+            this.ButtonSearch.UseVisualStyleBackColor = false;
+            this.ButtonSearch.Click += new System.EventHandler(this.ButtonSearch_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(99, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "ASTRONOMICAL PROCESSING";
+            // 
+            // ButtonAutofill
+            // 
+            this.ButtonAutofill.ForeColor = System.Drawing.Color.Green;
+            this.ButtonAutofill.Location = new System.Drawing.Point(356, 30);
+            this.ButtonAutofill.Name = "ButtonAutofill";
+            this.ButtonAutofill.Size = new System.Drawing.Size(75, 23);
+            this.ButtonAutofill.TabIndex = 9;
+            this.ButtonAutofill.Text = "AutoFill";
+            this.ButtonAutofill.UseVisualStyleBackColor = true;
+            this.ButtonAutofill.Click += new System.EventHandler(this.ButtonAutofill_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip.Location = new System.Drawing.Point(0, 463);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(456, 22);
+            this.statusStrip.TabIndex = 10;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(53, 17);
+            this.toolStripStatusLabel1.Text = "Message";
+            // 
+            // AstronomicalProcessing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(456, 485);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.ButtonAutofill);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ButtonSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ButtonSort);
+            this.Controls.Add(this.ButtonDelete);
+            this.Controls.Add(this.ButtonModify);
+            this.Controls.Add(this.ButtonAdd);
+            this.Controls.Add(this.TextBoxHours);
+            this.Controls.Add(this.ListBoxHours);
+            this.Name = "AstronomicalProcessing";
+            this.Text = "AstronomicalProcessing";
+            this.Load += new System.EventHandler(this.AstronomicalProcessing_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.AstronomicalProcessing_MouseMove);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBoxHours;
+        private System.Windows.Forms.TextBox TextBoxHours;
+        private System.Windows.Forms.Button ButtonAdd;
+        private System.Windows.Forms.Button ButtonModify;
+        private System.Windows.Forms.Button ButtonDelete;
+        private System.Windows.Forms.Button ButtonSort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ButtonSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ButtonAutofill;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ AstronomicalProcessing.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+// Atit Singh/Satbir Singh,Team:CSharpPerth, Sprint 1
+// Date: 15/09/2021
+// Version:1
+// AstronomicalProcessing
+// This is a form-based application which displays the hours in a listbox, user can able to add, delete and search the data.
+// Inputs, Processes, Outputs
+//ACRONYMS: SP1:Sprint1, CR: Client Requirement , PR: functional requirement, NFR: Non-functional requirement
+namespace AstronomicalProcessing
+{
+    //CR: Name of the application should be Astronomical processing
+    public partial class AstronomicalProcessing : Form
+    {
+        #region BUILTIN
+        public AstronomicalProcessing()
+        {
+            InitializeComponent();
+        }
+        private void AstronomicalProcessing_Load(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region DECLARING ARRAY, DISPLAY METHOD, FILL RANDOM NUMBERS
+
+        //SP1: CR: Application has integer array having element of 24
+        static int maxNumber = 24;
+        //FR : array must be Integer
+        int[] hours = new int[maxNumber];
+        int nextIndex = 0;
+        private void DisplayNumber()
+        {
+
+            ListBoxHours.Items.Clear();
+            for (int i = 0; i < nextIndex; i++)
+            {
+                ListBoxHours.Items.Add(hours[i]);
+            }
+        }
+
+        private void ListBoxHours_Click(object sender, EventArgs e)
+        {
+            int current = int.Parse(ListBoxHours.SelectedItem.ToString());
+            int idx = ListBoxHours.Items.IndexOf(current);
+            TextBoxHours.Text = hours[idx].ToString();
+        }
+
+        private void ButtonAutofill_Click(object sender, EventArgs e)
+        {
+            //FR:1 To display the random number from 10 to 99.
+            nextIndex = 24;
+            Random rand = new Random();
+            for (int i = 0; i < nextIndex; i++)
+            {
+                hours[i] = rand.Next(10, 100);
+            }
+
+            DisplayNumber();
+        }
+        #endregion
+
+        #region ADD, MODIFY AND DELETE
+
+        //CR/FR:2 Input textbox so that user can add, edit and delete the data 
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TextBoxHours.Text))
+            {
+                try
+                {
+                    int number = int.Parse(TextBoxHours.Text);
+                    hours[nextIndex] = number;
+                    nextIndex++;
+                    DisplayNumber();
+                    TextBoxHours.Clear();
+                    TextBoxHours.Focus();
+                }
+                catch(IndexOutOfRangeException)
+                {
+                    MessageBox.Show("Sorry,List exceed the limit of 24 numbers");
+                }
+                catch (OverflowException)
+                {
+                    MessageBox.Show("Sorry,Number too big to be accepted");
+                }
+
+            }
+            // FR:2 The program must generate an error message if the text box is empty.
+            else
+            {
+                MessageBox.Show("Blank space detected!Please input number in the Box.");
+            }
+
+        }
+
+        private void ButtonModify_Click(object sender, EventArgs e)
+        {
+
+            if (ListBoxHours.SelectedIndex != -1)
+            {
+                int current = int.Parse(ListBoxHours.SelectedItem.ToString());
+                int idx = ListBoxHours.Items.IndexOf(current);
+                DialogResult modifyTask = MessageBox.Show("Do you want to Continue?", "It will permanently modify the data.", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (modifyTask == DialogResult.Yes)
+                {
+                    int.TryParse(TextBoxHours.Text, out hours[idx]);
+                    DisplayNumber();
+                    TextBoxHours.Clear();
+                    TextBoxHours.Focus();
+                }
+                else
+                {
+
+                    toolStripStatusLabel1.Text = "User had cancelled to modify";
+
+                }
+
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "User need to select data from the list";
+            }
+        }
+
+        private void ButtonDelete_Click(object sender, EventArgs e)
+        {
+            if (ListBoxHours.SelectedIndex != -1)
+            {
+                int current = int.Parse(ListBoxHours.SelectedItem.ToString());
+                int idx = ListBoxHours.Items.IndexOf(current);
+                DialogResult delTask = MessageBox.Show("Do you want to Continue?", "It will permanently Delete the data.", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (delTask == DialogResult.Yes)
+                {
+                    hours[idx] = hours[nextIndex - 1];
+                    toolStripStatusLabel1.Text = "Number has been Deleted";
+                    nextIndex--;
+                    DisplayNumber();
+                    TextBoxHours.Clear();
+                    TextBoxHours.Focus();
+                }
+                else
+                {
+
+                    toolStripStatusLabel1.Text = "User had cancelled to Delete";
+
+                }
+
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "User need to select data from the list";
+            }
+        }
+        #endregion
+        #region Sort and Search
+        //FR:3 sort method using the Bubble Sort algorithm.Sorting in Ascending order
+        private void Sort()
+        {
+            for (int i = 0; i < nextIndex; i++)
+            {
+                for (int j = i + 1; j < nextIndex; j++)
+                {
+
+                    if (hours[i].CompareTo(hours[j]) > 0)
+                    {
+                        int temp = hours[i];
+                        hours[i] = hours[j];
+                        hours[j] = temp;
+                    }
+                }
+            }
+            DisplayNumber();
+        }
+        //CR: Button to sort
+        private void ButtonSort_Click(object sender, EventArgs e)
+        {
+            Sort();
+        }
+
+        //CR: Button to search Data
+        //FR:5 search method using the Binary Search algorithm.
+        private void ButtonSearch_Click(object sender, EventArgs e)
+        {
+
+            Sort();
+            int target = int.Parse(TextBoxHours.Text);
+            int min = 0;
+            int max = nextIndex - 1;
+            int mid = 0;
+            bool found = false;
+
+            while (min <= max)
+            {
+
+                mid = (min + max) / 2;
+                if (target == hours[mid])
+                {
+                    found = true;
+                    break;
+                }
+                else if (target.CompareTo(hours[mid]) < 0)
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            //FR:5 Code must generate a message if the search is successful.
+            if (found)
+            {
+                MessageBox.Show(target + " number is found");
+            }
+            //FR:5 Code must generate a message if the search is not successful.
+            else
+            {
+                MessageBox.Show("Number not found");
+            }
+
+        }
+        #endregion
+
+
+
+
+        private void AstronomicalProcessing_MouseMove(object sender, MouseEventArgs e)
+        {
+            toolStripStatusLabel1.Text = "";
+        }
+
+        private void TextBoxHours_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar) && !char.IsControl(e.KeyChar))
+                e.Handled = true;
+            toolStripStatusLabel1.Text = "Only digit accepted, not letter or alphabet";
+        }
+    }
+
+
+
+}
